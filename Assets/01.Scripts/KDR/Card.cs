7d248a9e7dd4046b2b7249ddef98d09a1a120f3f
dongdong5899 <dong012673@gmail.com>
2024-11-07 14:32:20 +0900

    .

--- Assets/01.Scripts/KDR/Card.cs
+++ Assets/01.Scripts/KDR/Card.cs
@@ -1,6 +1,10 @@
-using NUnit.Framework;
+using DG.Tweening;
 using System.Collections.Generic;
+using System.Security.Claims;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.WSA;
 
 [System.Serializable]
 public enum Curse
@@ -21,10 +25,87 @@ public enum Blessing
     Penance,
 }
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public List<Curse> curse;
     public List<Blessing> blessing;
 
-    public int Cost { get; private set; }
+    public int cost;
+    public int id;
+    public bool onMouse;
+    public bool isSelected;
+    public bool isUseable;
+
+    public RectTransform VisualTrm { get; private set; }
+    private RectTransform _rectTrm;
+
+    private Vector2 _startOffset;
+    private Vector3 _onMouseScale = Vector3.one * 1.2f;
+    private float _visualDefaultYPos;
+
+    private Vector2 _startPos;
+    private Quaternion _startRot;
+
+    private CardController _holder;
+
+    public void Init(CardController holder, int id)
+    {
+        this.id = id;
+        _holder = holder;
+        _rectTrm = transform as RectTransform;
+        VisualTrm = transform.Find("Visual") as RectTransform;
+    }
+
+    public void Use()
+    {
+        Debug.Log("Use!");
+        Destroy(gameObject);
+    }
+
+    public void SetUseable(bool value)
+    {
+        if (isUseable == value) return;
+        isUseable = value;
+        VisualTrm.GetComponent<Image>().color = value ? Color.yellow : Color.white;
+    }
+
+    public void SetStartValue()
+    {
+        _visualDefaultYPos = VisualTrm.anchoredPosition.y;
+        _startPos = _rectTrm.anchoredPosition;
+        _startRot = transform.rotation;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position - _startOffset;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onMouse = true;
+        VisualTrm.DOScale(_onMouseScale, 0.1f);
+        VisualTrm.DOAnchorPosY(_visualDefaultYPos + 100f, 0.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onMouse = false;
+        VisualTrm.DOScale(Vector3.one, 0.1f);
+        VisualTrm.DOAnchorPosY(_visualDefaultYPos, 0.1f);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isSelected = true;
+        _holder.SetSelectCard(this, true);
+        _startOffset = eventData.position - (Vector2)transform.position;
+        Debug.Log("End");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isSelected = false;
+        _holder.SetSelectCard(this, false);
+    }
 }
