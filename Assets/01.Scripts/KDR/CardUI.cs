3dd492fb77b11ee85c9b4c2e83d669002ad2595e
dongdong5899 <dong012673@gmail.com>
2024-11-08 09:44:57 +0900

    EnvyTag and PenanceTag

--- Assets/01.Scripts/KDR/CardUI.cs
+++ Assets/01.Scripts/KDR/CardUI.cs
@@ -31,6 +31,7 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
     public bool OnSelected { get; private set; }
     public bool IsSelectable { get; private set; }
+    public int LockedLevel { get; private set; } = 0;
     public bool IsHolded { get; private set; }
     public bool IsUseable { get; private set; }
     public bool IsFront { get; private set; }
@@ -48,7 +49,10 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
     private TextMeshProUGUI _nameText;
     private TextMeshProUGUI _descriptionText;
     private Image _image;
+    private Transform _curseTags;
+    private Transform _blessingTags;
 
+    private int _cardIndex;
     private Vector2 _startOffset;
     private Vector3 _onMouseScale = Vector3.one * 1.2f;
 
@@ -56,9 +60,10 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
     private Sequence _turnSeq;
 
-    public void Init(CardController holder)
+    public void Init(CardController controller, int index)
     {
-        _contoller = holder;
+        _cardIndex = index;
+        _contoller = controller;
         VisualTrm = transform.Find("Visual") as RectTransform;
         VisualImage = VisualTrm.GetComponent<Image>();
         _elementsTrm = VisualTrm.Find("Elements");
@@ -66,6 +71,8 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
         _nameText = _elementsTrm.Find("Name").GetComponent<TextMeshProUGUI>();
         _descriptionText = _elementsTrm.Find("Description").GetComponent<TextMeshProUGUI>();
         _image = _elementsTrm.Find("Image").GetComponent<Image>();
+        _curseTags = _elementsTrm.Find("CurseTag");
+        _blessingTags = _elementsTrm.Find("BlessingTag");
 
         _costText.text = cardSO.cost.ToString();
         _nameText.text = cardSO.name;
@@ -74,6 +81,35 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
         IsFront = false;
         _elementsTrm.gameObject.SetActive(false);
+
+        UpdateTag();
+    }
+
+    public void AfterInit()
+    {
+        if (curse.Contains(Curse.Envy))
+        {
+            if (_cardIndex - 1 >= 0) _contoller.cards[_cardIndex - 1].Lock(true);
+            if (_cardIndex + 1 < _contoller.cards.Count) _contoller.cards[_cardIndex + 1].Lock(true);
+        }
+    }
+
+    public void UpdateTag()
+    {
+        foreach (Curse tag in curse)
+        {
+            _curseTags.GetChild((int)tag).gameObject.SetActive(true);
+        }
+        foreach (Blessing tag in blessing)
+        {
+            _blessingTags.GetChild((int)tag).gameObject.SetActive(true);
+        }
+    }
+
+    public void Lock(bool isLock)
+    {
+        LockedLevel += isLock ? 1 : -1;
+        VisualImage.color = LockedLevel != 0 ? Color.gray : Color.white;
     }
 
     public void Turn(bool isFront)
@@ -87,21 +123,28 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
         _turnSeq.Append(VisualTrm.DOScaleX(0, 0.1f))
             .AppendCallback(() =>
             {
-                _elementsTrm.gameObject.SetActive(IsFront);
+                _elementsTrm.gameObject.SetActive(isFront);
                 VisualImage.sprite = isFront ? _forntSprite : _backSprite;
             })
             .Append(VisualTrm.DOScaleX(1, 0.1f))
             .AppendCallback(() => IsSelectable = true);
     }
 
-    public void SetCost(int cost)
+    public void CostModify(int cost, Color color = default)
     {
-        _costText.text = cost.ToString();
+        _costText.text = Mathf.Clamp(cardSO.cost + cost, 0, 10).ToString();
+        if (color != default)
+            _costText.color = color;
     }
 
     public void Use()
     {
         if (blessing.Contains(Blessing.Penance)) _contoller.AddCard(cardSO, true);
+        if (curse.Contains(Curse.Envy))
+        {
+            if (_cardIndex - 1 >= 0) _contoller.cards[_cardIndex - 1].Lock(false);
+            if (_cardIndex + 1 < _contoller.cards.Count) _contoller.cards[_cardIndex + 1].Lock(false);
+        }
         Debug.Log("Use!");
         Destroy(gameObject);
     }
@@ -112,18 +155,20 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
         if (IsUseable != newValue)
         {
             IsUseable = newValue;
-            VisualTrm.GetComponent<Image>().color = IsUseable ? Color.yellow : Color.white;
+            VisualImage.color = IsUseable ? Color.blue : Color.white;
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (LockedLevel != 0) return;
         transform.position = eventData.position - _startOffset;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (IsSelectable == false) return;
+        if (LockedLevel != 0) return;
+        if (IsFront == false || IsSelectable == false || IsHolded) return;
         OnSelected = true;
         VisualTrm.DOScale(_onMouseScale, 0.1f);
         //VisualTrm.DOAnchorPosY(_visualDefaultYPos + 100f, 0.1f);
@@ -131,7 +176,8 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (IsSelectable == false) return;
+        if (LockedLevel != 0) return;
+        if (IsFront == false || IsSelectable == false || IsHolded) return;
         OnSelected = false;
         VisualTrm.DOScale(Vector3.one, 0.1f);
         //VisualTrm.DOAnchorPosY(_visualDefaultYPos, 0.1f);
@@ -139,6 +185,7 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (LockedLevel != 0) return;
         IsHolded = true;
         _contoller.SetSelectCard(this, true);
         _startOffset = eventData.position - (Vector2)transform.position;
@@ -146,6 +193,7 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (LockedLevel != 0) return;
         IsHolded = false;
         _contoller.SetSelectCard(this, false);
     }
